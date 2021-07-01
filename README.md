0d9df1ba8314b0a240abb124183ee04fec8beafa
Dmitry Purtov <dmtr.pv@gmail.com>
2021-07-02 02:25:53 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# LensKit
+
+A description of this package.
---
+++ Tests/LensKitTests/LensKitTests.swift
@@ -0,0 +1,11 @@
+    import XCTest
+    @testable import LensKit
+
+    final class LensKitTests: XCTestCase {
+        func testExample() {
+            // This is an example of a functional test case.
+            // Use XCTAssert and related functions to verify your tests produce the correct
+            // results.
+            XCTAssertEqual(LensKit().text, "Hello, World!")
+        }
+    }
